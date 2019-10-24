24ff3aa2994077c2975c708a69c02fb39646aa3c
BRUNO  ALESSANDRO BOMBO <34913@ETECPAOF.LAN>
2019-10-23 22:37:41 -0300

    alterações

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-let urlAPI = 'https://gilsonpolito-api.herokuapp.com/alunos'
+let urlAPI = 'https://gilsonpolito-api.herokuapp.com/alunos/'
 
 $(document).ready(function(){
 
@@ -7,10 +7,11 @@ $(document).ready(function(){
         contentType: 'application/json' ,
         url: urlAPI,
         dataType: 'json' ,
-        success: function(data, textStatus, jqXHR){
+        success: function(data){
             $.each(data, function(index, itemData){
                 insereLinha(itemData.id, itemData.nome, itemData.site);
-            });    
+            });
+            handler();    
         },
         error: function(jqXHR, textStatus, errorThrown){
             alert('Status: ' + textStatus + '\nTipo:' + errorThrown + '\nMensagem: ' + jqXHR.responseText);
@@ -46,14 +47,63 @@ $('#add-to-list') .on('click',(evento)=>{
         url: urlAPI,
         dataType: 'json',
         data: formToJSON(),
-        success: function(data, textStatus, jqXHR){
-            insereLinha(data.id, data.nome, data.site)
+        success: function(data){
+            insereLinha(data.id, data.nome, data.site);
+            handler();
         },
         error: function(jqXHR, textStatus, errorThrown){
             alert('Status: ' + textStatus + '\nTipo:' + errorThrown + '\nMensagem: ' + jqXHR.responseText);
         }
     })
-})
+});
+
+function handler(){
+
+    $('.action_delete').each(function(){
+        $(this).click(function(evento){
+            evento.stopImmediatePropagation();
+            evento.preventDefault();
+            let tr = $(this).parent().parent()
+            if(confirm('Deseja remover o aluno?')){
+                $.ajax({
+                    type: 'DELETE' ,
+                    contentType: 'application/json' ,
+                    url: urlAPI + $(this).attr('value'),
+                    success: function(){
+                        tr.remove();
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        alert('Status: ' + textStatus + 
+                            '\nTipo:' + errorThrown + 
+                            '\nMensagem: ' + jqXHR.responseText);
+                    },
+                });
+            }
+        });
+    });
+    
+    $('.action_edit').each(function(){
+        $(this).click(function(evento){
+            evento.preventDefault();
+            $.ajax({
+                type: 'GET' ,
+                contentType: 'application/json' ,
+                url: urlAPI + $(this).attr('value') ,
+                success: function(data){
+                    $('#idHidden').val(data.id);
+                    $('#nomeId').val(data.nome);
+                    $('#emailId').val(data.site);
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    alert('Status: ' + textStatus + 
+                        '\nTipo:' + errorThrown + 
+                        '\nMensagem: ' + jqXHR.responseText);
+                },
+            });
+        });
+    });
+
+}
 
 function formToJSON(){
     return JSON.stringify({
